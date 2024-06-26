bae71fb523b42a4f80e8e5767ee8235440e93f83
parkab27 <pkabaria77@gmail.com>
2024-06-26 10:48:12 -0400

    bootstrap setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { InternshipsComponent } from "./internships/internships.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HeaderComponent, InternshipsComponent]
 })
 export class AppComponent {
   title = 'internship-tracker';
