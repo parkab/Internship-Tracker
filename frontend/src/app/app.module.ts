11eb298e9464b1ea739f74d895cad8037496fb48
parkab27 <pkabaria77@gmail.com>
2024-08-22 15:29:44 -0400

    auth error fix

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { BrowserModule } from '@angular/platform-browser';
+import { AuthInterceptor } from './services/auth.interceptor';
 
 @NgModule({
   imports: [
@@ -14,6 +15,13 @@ import { BrowserModule } from '@angular/platform-browser';
   ],
   exports: [
     MatSlideToggleModule
-  ]
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
 })
 export class AppModule { }
