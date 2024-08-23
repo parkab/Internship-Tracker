65ea7cb4bc63f23d9cc9a95cb55ef22a31f99c7a
parkab27 <pkabaria77@gmail.com>
2024-08-22 22:29:15 -0400

    vercel fix

--- frontend/dist/internship-tracker/server/main.server.mjs
+++ frontend/dist/internship-tracker/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-VVIUAHTK.mjs";import"./chunk-K64YC5WB.mjs";import"./chunk-UQFOLXIE.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-M7AD4GTU.mjs";import"./chunk-K64YC5WB.mjs";import"./chunk-UQFOLXIE.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
